7472b3af1ab3d3d2be85df39f0c279657f787029
Ramakrushna-Karoshi <karoshiramakrushna@gmail.com>
2025-07-27 22:37:53 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -8,7 +8,12 @@ def size(cms):
         return 'L'
 
 
+
 assert(size(37) == 'S')
 assert(size(40) == 'M')
 assert(size(43) == 'L')
+
+# New test case to make it fail
+assert(size(38) == 'S') # Expecting 'S' or 'M', but it will return 'L'
+
 print("All is well (maybe!)")
