4947f34413323e6a385bc561c7265621c1e46cdd
Ramakrushna-Karoshi <karoshiramakrushna@gmail.com>
2025-07-31 00:02:35 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -16,6 +16,23 @@
 
 
 
+# Task 1 :OK
+# def size(cms):
+#     if cms < 38:
+#         return 'S'
+#     elif cms > 38 and cms < 42:
+#         return 'M'
+#     else:
+#         return 'L'
+
+
+# # New test case to make it fail
+# assert(size(38) == 'S') # Expecting 'S' or 'M', but it will return 'L'
+# print("All is well (maybe!)")
+
+
+# Task2 : ok
+# Production code  : 
 def size(cms):
     if cms < 38:
         return 'S'
@@ -25,7 +42,20 @@ def size(cms):
         return 'L'
 
 
-# New test case to make it fail
-assert(size(38) == 'S') # Expecting 'S' or 'M', but it will return 'L'
-print("All is well (maybe!)")
+# test code 
+from size_module import size
+
+def test_size_edge_case():
+    assert size(38) == 'S', "Expected size(38) to return 'S', but got something else"
+
+def test_all_cases():
+    assert size(36) == 'S'
+    assert size(39) == 'M'
+    assert size(43) == 'L'
+
+if __name__ == "__main__":
+    test_size_edge_case()
+    test_all_cases()
+    print("All tests completed.")
+
 
